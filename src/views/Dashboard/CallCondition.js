0927aaa80505a71554f56de456de5a55522a0195
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-08-31 15:20:40 +0300

    form updating

--- src/views/Dashboard/CallCondition.js
+++ src/views/Dashboard/CallCondition.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default function CallCondition() {
+  const [hidden, setHidden] = useState(false);
+  const [value, setValue] = React.useState("New Contact");
+  const handleChangeCallType = (event) => {
+    setValue(event.target.value);
+  };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    if (event.target.value === "New Contact") {
+      setHidden(false);
+    }
+    if (event.target.value === "Existing Contact") {
+      setHidden(true);
+    }
+  };
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-      Call conditions
+        Call conditions
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={12}>
@@ -22,8 +40,67 @@ export default function CallCondition() {
             autoComplete="given-name"
           />
         </Grid>
-
         <Grid item xs={12}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend"> Who will you call?</FormLabel>
+            <RadioGroup
+              aria-label="contact"
+              name="contact"
+              value={value}
+              onChange={handleChange}
+            >
+              <FormControlLabel
+                value="New Contact"
+                control={<Radio />}
+                label="New Contact"
+              />
+              <FormControlLabel
+                value="Existing Contact"
+                control={<Radio />}
+                label=" Existing Contact"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        {!hidden && (
+
+            <Grid item xs={12}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  {" "}
+                  What kind of call will you do?
+                </FormLabel>
+                <RadioGroup
+                  aria-label="callType"
+                  name="callType"
+                  value={value}
+                  onChange={handleChangeCallType}
+                >
+                  <FormControlLabel
+                    value="Initial Call"
+                    control={<Radio />}
+                    label="Initial Call"
+                  />
+                  <FormControlLabel
+                    value="Follow up call"
+                    control={<Radio />}
+                    label="Follow up call"
+                  />
+                  <FormControlLabel
+                    value="Customer call"
+                    control={<Radio />}
+                    label="Customer call"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+          
+        )}
+
+        {hidden && <div>old</div>}
+
+        {/*
+   <Grid item xs={12}>
           <TextField
             required
             id="address1"
@@ -81,6 +158,8 @@ export default function CallCondition() {
             label="Use this address for payment details"
           />
         </Grid>
+     
+*/}
       </Grid>
     </React.Fragment>
   );
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -120,12 +120,10 @@ export default function Dashboard() {
             {activeStep === steps.length ? (
               <React.Fragment>
                 <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
+                  Congrates :)
                 </Typography>
                 <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
+                  Your form has been successfylly submittd 
                 </Typography>
               </React.Fragment>
             ) : (
